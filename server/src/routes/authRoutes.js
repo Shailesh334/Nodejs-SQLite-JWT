96792fd388bb71b6132bd7d97c4e8487cc7532d8
shailesh_334 <shaileshgurle334@gmail.com>
2025-09-19 21:59:47 +0530

    Changed to React FrontEnd

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import db from '../db.js'
+
+
+
+const router = express.Router();
+
+
+
+
+router.post('/register' , (req , res)=>{
+
+        const {username , password} = req.body;
+
+        // Encrypting password
+        const hashedPassword = bcrypt.hashSync(password , 8);
+
+        // save the new user and hashed password in db
+
+        try{
+                const insertUser = db.prepare(`INSERT INTO users(username , password) VALUES (? , ?)`);
+                const data = insertUser.run(username , hashedPassword);
+            
+            // now that we have user in db insert default task for this user in todo db
+                const defaultTodo = `Hello :) Add your first task `;
+                const insertTodo = db.prepare(`INSERT INTO todos(user_id , task) VALUES ( ? , ?)`);
+                insertTodo.run(data.lastInsertRowid , defaultTodo);
+                
+
+            // create token for this user and send it to frontend
+                const token = jwt.sign({id:data.lastInsertRowid} , process.env.JWT_SECRET_KEY , {expiresIn: '24h'});
+                res.json({token});
+
+        }
+        catch(err){
+                console.log(err.message);
+                res.sendStatus(503)
+        }
+
+
+})
+
+
+
+
+router.post('/login' , (req , res)=>{
+
+        const {username , password } = req.body;
+
+        try{
+        // Get the user
+            const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
+            const user = getUser.get(username);
+
+        // if user doesnt exists return error
+            if(!user)return res.status(404).send({message : "User not found"})
+
+        // Check the entered password matches the password in db
+            const isPasswordValid = bcrypt.compareSync(password , user.password)
+        
+        // If the password does not match return error
+            if(!isPasswordValid) return res.status(401).send({ message: "Invalid password" });
+
+        // then we have a successful authentication
+            const token = jwt.sign({id : user.id} , process.env.JWT_SECRET_KEY , {expiresIn: '24h'})
+            res.json({token});
+        }
+        catch(err){
+            console.log(err.message);
+            res.sendStatus(503)
+        }
+
+})
+
+
+
+export default router;
---
+++ server/src/routes/todoRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import db from '../db.js'
+
+const router = express.Router()
+
+// Get all todos for logged-in user
+router.get('/', (req, res) => {
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
+})
+
+
+export default router;
